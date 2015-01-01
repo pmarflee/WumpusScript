5259168e7b6926fdf38004d80ce4e44ad05e11b0
Paul Marfleet <paul.marfleet@gmail.com>
2015-01-01 15:45:08 +0000

    Implemented parameterised tests for asserting correct Cave layout

--- WumpusScript/Scripts/Tests/ModelTests.ts
+++ WumpusScript/Scripts/Tests/ModelTests.ts
@@ -19,6 +19,43 @@ export class CaveTests extends tsUnit.TestClass {
 
 }
 
+export class CaveRoomsHaveCorrectExitsTests extends tsUnit.TestClass {
+    private _cave: Cave = new Cave();
+
+    constructor() {
+        super();
+
+        var data: [number, number[]][] = [
+            [0, [1, 4, 7]],
+            [1, [0, 2, 9]],
+            [2, [1, 3, 11]],
+            [3, [2, 4, 13]],
+            [4, [0, 3, 5]],
+            [5, [4, 6, 14]],
+            [6, [5, 7, 16]],
+            [7, [0, 6, 8]],
+            [8, [7, 9, 17]],
+            [9, [1, 8, 10]],
+            [10, [9, 11, 18]],
+            [11, [2, 10, 12]],
+            [12, [11, 13, 19]],
+            [13, [3, 12, 14]],
+            [14, [5, 13, 15]],
+            [15, [14, 16, 19]],
+            [16, [6, 15, 17]],
+            [17, [8, 16, 18]],
+            [18, [10, 17, 19]],
+            [19,[12,15,18]]
+        ];
+
+        this.parameterizeUnitTest(this.roomHasCorrectExits, data);
+    }
+
+    roomHasCorrectExits(roomNumber: number, expected: number[]) {
+        this.areCollectionsIdentical(expected, this._cave.rooms[roomNumber].exits);
+    }
+}
+
 export class PlayerHazardTests extends tsUnit.TestClass {
 
     private _cave: Cave;
